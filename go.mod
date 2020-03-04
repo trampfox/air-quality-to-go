6f995e51817bb107cefbe1f28a97d80eee2c3261
davide.monfrecola <davide.monfrecola@adevinta.com>
2020-03-04 22:39:49 +0100

    Add test functions

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/trampfox/air-quality-to-go
 
 go 1.13
 
-require github.com/prometheus/common v0.9.1
+require (
+	github.com/go-kit/kit v0.9.0
+	github.com/prometheus/common v0.9.1
+	google.golang.org/api v0.20.0
+)
--- main.go
+++ main.go
@@ -6,29 +6,53 @@ import (
 	"io/ioutil"
 	"net/http"
 
-	"github.com/prometheus/common/log"
 	"github.com/trampfox/air-quality-to-go/internal/scraper"
 )
+
+type ObjectValue struct {
+	PrObject         float64 `json:"prObject"`
+	NumberOfDecimals int     `json:"numberOfDecimals"`
+	Bold             bool    `json:"bold"`
+	Italic           bool    `json:"italic"`
+	Underlined       bool    `json:"underlined"`
+	FixedWidth       bool    `json:"fixedWidth"`
+	Center           bool    `json:"center"`
+	NoWrap           bool    `json:"noWrap"`
+	Highlighted      bool    `json:"highlighted"`
+}
+
+type PollutionEntry struct {
+	Date          string
+	PollutantName string
+	DailyValues   []DailyValue
+}
+
+type DailyValue struct {
+	Location               string
+	MaxHourlyValue         string
+	MaxHourlyValueDateTime string
+}
 
 func main() {
 	response, err := http.Get("http://www.cittametropolitana.torino.it/js/ariaweb/reports/rs-20200224.json")
 	if err != nil {
-		log.Error(err)
+		panic(err)
 	}
 	defer response.Body.Close()
 
 	contents, err := ioutil.ReadAll(response.Body)
 	if err != nil {
-		log.Error(err)
+		panic(err)
 	}
 
 	var entries scraper.RawDailyEntry
 	err = json.Unmarshal([]byte(contents), &entries)
 	if err != nil {
-		log.Error(err)
+		panic(err)
 	}
 
-	fmt.Printf("%v\n", entries[0])
+	pollutantNames(entries)
+	fmt.Printf("%v", measuringStations(entries))
 
 	// for _, entry := range entries {
 	// 	fmt.Printf("%v\n", entry.HeaderLines)
@@ -36,4 +60,34 @@ func main() {
 
 	// fmt.Printf("%v\n", entries)
 	// fmt.Printf("%s\n", string(contents))
+}
+
+func pollutantNames(rawEntries scraper.RawDailyEntry) []PollutionEntry {
+	var pEntries []PollutionEntry
+	for _, rawEntry := range rawEntries {
+		if pName := rawEntry.HeaderLines[0].PrObject; pName != "" {
+			pEntry := PollutionEntry{
+				PollutantName: rawEntry.HeaderLines[0].PrObject,
+			}
+			pEntries = append(pEntries, pEntry)
+		}
+	}
+	fmt.Printf("%v\n", pEntries)
+	return pEntries
+}
+
+func measuringStations(rawEntries scraper.RawDailyEntry) []DailyValue {
+	var dailyValues []DailyValue
+	for _, rawEntry := range rawEntries {
+		// fmt.Printf("%v\n", rawEntry.Rows)
+		for i := 2; i < len(rawEntry.Rows); i++ {
+			dailyValue := DailyValue{
+				Location:       string(rawEntry.Rows[i][0]),
+				MaxHourlyValue: string(rawEntry.Rows[i][1]),
+			}
+			dailyValues = append(dailyValues, dailyValue)
+		}
+	}
+
+	return dailyValues
 }
