f39db0429abe10e3e1d984b2a891b9e3546162a2
davide.monfrecola <davide.monfrecola@adevinta.com>
2020-03-04 21:56:15 +0100

    Improve unmarshal and add RawTailLine struct

--- internal/scraper/airquality.go
+++ internal/scraper/airquality.go
@@ -3,28 +3,17 @@ package scraper
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"strconv"
 )
 
 type RawDailyEntry []struct {
-	NumColumns      int        `json:"numColumns"`
-	Rows            [][]Row `json:"rows"`
-	MarkFirstRow    bool       `json:"markFirstRow"`
-	MarkFirstColumn bool       `json:"markFirstColumn"`
-	HeaderLines     []struct {
-		PrObject         string `json:"prObject"`
-		NumberOfDecimals int    `json:"numberOfDecimals"`
-		FontType         string `json:"fontType"`
-		FontDimension    int    `json:"fontDimension"`
-		Bold             bool   `json:"bold"`
-		Italic           bool   `json:"italic"`
-		Underlined       bool   `json:"underlined"`
-		FixedWidth       bool   `json:"fixedWidth"`
-		Center           bool   `json:"center"`
-		NoWrap           bool   `json:"noWrap"`
-		Highlighted      bool   `json:"highlighted"`
-	} `json:"headerLines"`
-	TailLines []interface{} `json:"tailLines"`
+	NumColumns      int              `json:"numColumns"`
+	Rows            [][]Row          `json:"rows"`
+	MarkFirstRow    bool             `json:"markFirstRow"`
+	MarkFirstColumn bool             `json:"markFirstColumn"`
+	HeaderLines     []HeaderLine     `json:"headerLines"`
+	TailLines       []RawTailLine `json:"tailLines"`
 }
 
 type ObjectValue struct {
@@ -39,29 +28,90 @@ type ObjectValue struct {
 	Highlighted      bool    `json:"highlighted"`
 }
 
-type Row string 
+type HeaderLine struct {
+	PrObject         string `json:"prObject"`
+	NumberOfDecimals int    `json:"numberOfDecimals"`
+	FontType         string `json:"fontType"`
+	FontDimension    int    `json:"fontDimension"`
+	Bold             bool   `json:"bold"`
+	Italic           bool   `json:"italic"`
+	Underlined       bool   `json:"underlined"`
+	FixedWidth       bool   `json:"fixedWidth"`
+	Center           bool   `json:"center"`
+	NoWrap           bool   `json:"noWrap"`
+	Highlighted      bool   `json:"highlighted"`
+}
+
+type TailLine struct {
+	PrObject         string `json:"prObject"`
+	NumberOfDecimals int    `json:"numberOfDecimals"`
+	FontType         string `json:"fontType"`
+	FontDimension    int    `json:"fontDimension"`
+	Bold             bool   `json:"bold"`
+	Italic           bool   `json:"italic"`
+	Underlined       bool   `json:"underlined"`
+	FixedWidth       bool   `json:"fixedWidth"`
+	Center           bool   `json:"center"`
+	NoWrap           bool   `json:"noWrap"`
+	Highlighted      bool   `json:"highlighted"`
+}
+
+type Row string
+type RawTailLine TailLine
 
 func (fr *Row) UnmarshalJSON(b []byte) error {
 	var s string
-	err := json.Unmarshal(b, &s)
-	if err == nil {
+	if err := json.Unmarshal(b, &s); err == nil {
 		*fr = Row(s)
 		return nil
 	}
 
 	var ov ObjectValue
-	err = json.Unmarshal(b, &ov)
-	if err == nil {
+	if err := json.Unmarshal(b, &ov); err == nil {
 		*fr = Row(fmt.Sprintf("%.0f", ov.PrObject))
 		return nil
 	}
 
-	var i int 
-	err = json.Unmarshal(b, &i)
-	if err == nil {
+	var hl HeaderLine
+	if err := json.Unmarshal(b, &hl); err == nil {
+		*fr = Row(hl.PrObject)
+		return nil
+	}
+
+	var tl TailLine
+	if err := json.Unmarshal(b, &tl); err == nil {
+		*fr = Row(tl.PrObject)
+		return nil
+	}
+
+	var i int
+	if err := json.Unmarshal(b, &i); err == nil {
 		*fr = Row(strconv.Itoa(i))
 		return nil
 	}
-	
-	return err
+
+	log.Printf("unknown type, skip element %s", string(b))
+
+	return nil
+}
+
+func (fr *RawTailLine) UnmarshalJSON(b []byte) error {
+
+	var s string
+	if err := json.Unmarshal(b, &s); err == nil {
+		*fr = RawTailLine{
+			PrObject: s,
+		}
+		return nil
+	}
+
+	var tl TailLine
+	if err := json.Unmarshal(b, &tl); err == nil {
+		*fr = RawTailLine(tl)
+		return nil
+	}
+
+	log.Printf("unknown type, skip element %s", string(b))
+
+	return nil
 }
